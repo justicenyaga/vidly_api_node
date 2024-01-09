659b879e7409fc867a0df369045aa867396b9b01
Justice Nyaga <justiceevansmwema@gmail.com>
2024-01-09 22:20:49 +0300

    Minor fixes

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,6 +1,6 @@
 const logger = require("../logger");
 
-module.exports = function (err, res, res, next) {
+module.exports = function (err, req, res, next) {
   logger.error(err.message, { metadata: err.stack });
   res.status(500).send("Something failed.");
 };
--- routes/rentals.js
+++ routes/rentals.js
@@ -2,7 +2,6 @@ const { Rental, validate } = require("../models/rental");
 const { Customer } = require("../models/customer");
 const { Movie } = require("../models/movie");
 const auth = require("../middlewares/auth");
-const admin = require("../middlewares/admin");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 const express = require("express");
@@ -49,38 +48,6 @@ router.post("/", auth, async (req, res) => {
   }
 
   session.endSession();
-});
-
-router.put("/:id", auth, async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const customer = await Customer.findById(req.body.customerId);
-  if (!customer) return res.status(400).send("Invalid customer");
-
-  const movie = await Movie.findById(req.body.movieId);
-  if (!movie) return res.status(400).send("Invalid movie");
-
-  const rental = await Rental.findByIdAndUpdate(
-    req.params.id,
-    {
-      customer: _.pick(customer, ["_id", "name", "phone", "isGold"]),
-      movie: _.pick(movie, ["_id", "title", "dailyRentalRate"]),
-    },
-    { new: true }
-  );
-  if (!rental)
-    return res.status(404).send("The rental with the given Id was not found");
-
-  res.send(rental);
-});
-
-router.delete("/:id", [auth, admin], async (req, res) => {
-  const rental = await Rental.findByIdAndDelete(req.params.id);
-  if (!rental)
-    return res.status(404).send("The rental with the given Id was not found");
-
-  res.send(rental);
 });
 
 router.get("/:id", async (req, res) => {
