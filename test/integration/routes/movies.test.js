6c5e781acc7fff4a7f18aa333864bf93bdba6a30
Justice Nyaga <justiceevansmwema@gmail.com>
2024-01-10 19:27:08 +0300

    Test the POST /api/movies endpoint

--- test/integration/routes/movies.test.js
+++ test/integration/routes/movies.test.js
@@ -8,6 +8,8 @@ const {
   it,
 } = require("@jest/globals");
 const { Movie } = require("../../../models/movie");
+const { Genre } = require("../../../models/genre");
+const { User } = require("../../../models/user");
 
 describe("/api/movies", () => {
   let server;
@@ -19,6 +21,7 @@ describe("/api/movies", () => {
   afterEach(async () => {
     await server.close();
     await Movie.deleteMany({});
+    await Genre.deleteMany({});
   });
 
   describe("GET /", () => {
@@ -74,6 +77,100 @@ describe("/api/movies", () => {
       const id = new mongoose.Types.ObjectId();
       const res = await request(server).get("/api/movies/" + id);
       expect(res.status).toBe(404);
+    });
+  });
+
+  describe("POST /", () => {
+    let token;
+    let title;
+    let dailyRentalRate;
+    let numberInStock;
+    let genreId;
+
+    beforeEach(async () => {
+      token = new User().generateAuthToken();
+      title = "movie1";
+      dailyRentalRate = 2;
+      numberInStock = 10;
+      genreId = new mongoose.Types.ObjectId();
+
+      await Genre.create({ _id: genreId, name: "genre1" });
+    });
+
+    const exec = () =>
+      request(server).post("/api/movies").set("x-auth-token", token).send({
+        title,
+        dailyRentalRate,
+        numberInStock,
+        genreId,
+      });
+
+    it("should return 401 if the client is not logged in", async () => {
+      token = "";
+      const res = await exec();
+      expect(res.status).toBe(401);
+    });
+
+    it("should return 400 if the title is less than 5 characters", async () => {
+      title = "a";
+      const res = await exec();
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 400 if the title is more than 50 characters", async () => {
+      title = new Array(52).join("a");
+      const res = await exec();
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 400 if dailyRentalRate is less than 0", async () => {
+      dailyRentalRate = -1;
+      const res = await exec();
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 400 if numberInStock is less than 0", async () => {
+      numberInStock = -1;
+      const res = await exec();
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 400 if an invalid genreId is passed", async () => {
+      genreId = "1";
+      const res = await exec();
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 400 if no genre exists for the given id", async () => {
+      genreId = new mongoose.Types.ObjectId();
+      const res = await exec();
+      expect(res.status).toBe(400);
+    });
+
+    it("should save the movie if the request is valid", async () => {
+      await exec();
+      const movie = await Movie.findOne({
+        title,
+        dailyRentalRate,
+        numberInStock,
+      });
+
+      const expected_movie = { title, dailyRentalRate, numberInStock };
+
+      expect(movie).toMatchObject(expected_movie);
+    });
+
+    it("should return the movie if the request is valid", async () => {
+      const res = await exec();
+      expect(Object.keys(res.body)).toEqual(
+        expect.arrayContaining([
+          "_id",
+          "title",
+          "dailyRentalRate",
+          "numberInStock",
+          "genre",
+        ])
+      );
     });
   });
 });
